4f5e8432713f7f198f4574d9f9ba8cc99dd0fc2d
hsjeong-desktop <hsjeong.cs@gmail.com>
2020-01-31 10:36:21 -0800

    2020/02/01

--- ER80/div2/C.cpp
+++ ER80/div2/C.cpp
@@ -1,12 +1,24 @@
 #include <cstdio>
-
+ 
 int N, M;
-
-int dp(int i, int x, int y) {
-    
+int dp[1020][21];
+#define MOD 1000000007
+ 
+int combination(int n, int r) {
+    if(n == r)
+        return dp[n][r] = 1;
+    if(r == 0)
+        return dp[n][r] = 1;
+    if(dp[n][r] != -1) 
+        return dp[n][r];
+    return dp[n][r] = (combination(n-1, r-1)%MOD + combination(n-1, r)%MOD)%MOD;
 }
-
+ 
 int main() {
     scanf("%d %d", &N, &M);
+    for(int i = 0; i <= N+2*M-1; ++i)
+        for(int j = 0; j <= 2*M; ++j)
+            dp[i][j] = -1;
+    printf("%d\n", combination(N+2*M-1, 2*M));
     return 0;
 }
--- R615/div3/C.cpp
+++ R615/div3/C.cpp
@@ -1,65 +1,26 @@
 #include <cstdio>
-#include <utility>
-#include <vector>
-#include <algorithm>
-using namespace std;
 
-int N;
-bool check[32000];
-vector<int> prime;
-
-int main()  {
-    for(int i = 2; i <= 32000; ++i) {
-        if(!check[i]) {
-            for(int j = 1; i*j <= 32000; ++j)
-                prime.push_back(i*j);
-        }
-    }
-    sort(prime.begin(), prime.end());
-    int TC;
+int main() {
+    int TC, N;
     scanf("%d", &TC);
     while(TC--) {
         scanf("%d", &N);
-        int size = (int)prime.size();
-        int idx = 0, pf = prime[idx];
-        int last, tmp = 1;
-        vector<pair<int, int> > v;
-        while(pf*pf <= N && idx < size) {
-            while(N % pf == 0) {
-                N /= pf;
-                if(v.size() == 0 || v[(int)v.size()-1].first != pf)
-                    v.push_back(pair<int, int>(pf, 1));
-                else
-                    v[(int)v.size()-1].second++;
-            }
-            pf = prime[++idx];
+        int a, b, c;
+        bool valid = false;
+        for(a = 2; a*a <= N; ++a) {
+            if(N % a == 0)
+                break;
         }
-        if(N != 1) {
-            if(v.size() == 0 || v[(int)v.size()-1].first != N)
-                v.push_back(pair<int, int>(N, 1));
-            else
-                v[(int)v.size()-1].second++;
+        for(b = a+1; b*b <= N/a; ++b) {
+            if((N/a) % b == 0)
+                break;
         }
-        size = (int)v.size();
-        vector<int> ans;
-        for(int i = 0; i < size && ans.size() < 3; ++i) {
-            int j = v[i].second, k = 1, x = v[i].first;
-            //printf("j: %d x: %d\n", j, x);
-            while(j > 0) {
-                ans.push_back(x);
-                x = x*v[i].first;
-                ++k;
-                j -= k;
-            }
-        }
-        if(ans.size() < 3) {
-            printf("NO\n");
-        }
-        else {
+        if(b > a && N/(a*b) > b) {
+            c = N/(a*b);
             printf("YES\n");
-            for(int i = 0; i < 3; ++i)
-                printf("%d ", ans[i]);
-            printf("\n");
+            printf("%d %d %d\n", a, b, c);
+        } else {
+            printf("NO\n");
         }
     }
     return 0;
