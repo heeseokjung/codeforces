3f6a81b1d72abf85a4a291448bf9b12114b075ec
hsjeong-desktop <hsjeong.cs@gmail.com>
2020-02-14 10:06:49 -0800

    2020/02/15

--- ER82/D.cpp
+++ ER82/D.cpp
@@ -1,10 +1,16 @@
 #include <cstdio>
 
+int M;
+long long N;
+long long arr[100000];
+
 int main() {
     int TC;
     scanf("%d", &TC);
     while(TC--) {
-
+        scanf("%lld %d", &N, &M);
+        for(int i = 0; i < N; ++i)
+            scanf("%lld", arr+i);
     }
     return 0;
 }
