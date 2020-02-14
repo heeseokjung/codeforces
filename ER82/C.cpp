fda9c3c29cd30e6dff35cf9cca95cfcde22602f7
hsjeong-desktop <hsjeong.cs@gmail.com>
2020-02-14 09:16:08 -0800

    2020/02/15

--- ER82/C.cpp
+++ ER82/C.cpp
@@ -1,56 +1,61 @@
 #include <iostream>
-#include <cstdio>
 #include <string>
-#include <vector>
 using namespace std;
+
+inline int absl(int x) {
+    return x < 0 ? -x : x;
+}
 
 int main() {
     int TC;
     cin >> TC;
+    bool used[26];
     while(TC--) {
         string s;
         cin >> s;
-        bool valid[26];
         for(int i = 0; i < 26; ++i)
-            valid[i] = false;
+            used[i] = false;
+        int pos = 0;
+        string keyboard;
+        keyboard = keyboard + s[pos];
+        used[s[pos]-'a'] = true;
+        bool is_possible = true;
         int size = (int)s.size();
-        bool check = false;
-        for(int i = 0; i < size; ++i) {
-            int j = i-1, k = i+1;
-            int count = 0;
-            while(j >= 0 && k < size && s[j] == s[k]) {
-                --j; ++k;
-                count++;
-            }
-            if(count > 0 && !check) {
-                if(j == -1) {
-                    check = true;
-                    cout << "YES\n";
-                    for(int x = j+1; x <= i; ++x) {
-                        cout << s[x];
-                        valid[s[x]-'a'] = true;
-                    }
-                    for(char x = 97; x < 97+26; ++x) {
-                        if(!valid[x-'a'])
-                            cout << x;
-                    }
-                    cout << "\n";
-                } else if(k == size) {
-                    check = true;
-                    for(int x = i; x <= k-1; ++x) {
-                        cout << s[x];
-                        valid[s[x]-'a'] = true;
-                    }
-                    for(char x = 97; x < 97+26; ++x) {
-                        if(!valid[x-'a'])
-                            cout << x;
-                    }
-                    cout << "\n";
+        for(int i = 1; i < size; ++i) {
+            if(used[s[i]-'a']) {
+                if(pos > 0 && keyboard[pos-1] == s[i]) {
+                    pos--;
+                } else if(pos < (int)keyboard.size()-1 && keyboard[pos+1] == s[i]) {
+                    pos++;
+                } else {
+                    cout << "NO\n";
+                    is_possible = false;
+                    break;
+                }
+            } else {
+                if(pos == 0) {
+                    keyboard = s[i] + keyboard;
+                    used[s[i]-'a'] = true;
+                } else if(pos == (int)keyboard.size()-1) {
+                    keyboard = keyboard + s[i];
+                    pos++;
+                    used[s[i]-'a'] = true;
+                } else {
+                    cout << "NO\n";
+                    is_possible = false;
+                    break;
                 }
             }
         }
-        if(!check)
-            cout << "NO\n";
+        if(is_possible) {
+            cout << "YES\n";
+            cout << keyboard;
+            for(int i = 0; i < 26; ++i) {
+                if(!used[i])
+                    cout << (char)(i + 'a');
+            }
+            cout << "\n";
+        }
     }
     return 0;
 }
