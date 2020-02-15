7de10405287f5e35b3ad3c3ceceba28cf91d952f
hsjeong-desktop <hsjeong.cs@gmail.com>
2020-02-15 10:17:32 -0800

    2020/02/16

--- R619DIV2/B.cpp
+++ R619DIV2/B.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 int N;
 int arr[100000];
-vector<pair<int, int> > v;
 
 inline long long absl(long long x) {
     return x < 0 ? -x : x;
@@ -17,39 +16,37 @@ int main() {
     scanf("%d", &TC);
     while(TC--) {
         scanf("%d", &N);
+        vector<int> v;
         int count = 0;
-        for(int i = 0; i < N; ++i) {
+        for(int i = 0; i < N; ++i)
             scanf("%d", arr+i);
+        for(int i = 0; i < N; ++i) { 
             if(arr[i] != -1) {
                 count++;
-                v.push_back(pair<int, int>(arr[i], i));
+                if(i > 0 && arr[i-1] == -1) {
+                    v.push_back(arr[i]);
+                }
+                else if(i < N-1 && arr[i+1] == -1) {
+                    v.push_back(arr[i]);
+                }
             }
         }
         if(count == 0) {
-            printf("0 %d\n", 42);
+            printf("0 42\n");
         } else if(count == 1) {
-            printf("0 %d\n", v[0].first);
+            printf("0 %d\n", v[0]);
         } else {
             sort(v.begin(), v.end());
-            //printf("v[0].first: %d v[count-1].first: %d count: %d\n", v[0].first, v[count-1].first, count);
-            long long k = 0;
-            for(int i = 0; i < count; ++i)
-                k += v[i].first;
-            k = k / (long long)count;
-            long long m = 0;
+            int k = (v[0] + v[(int)v.size()-1]) / 2;
+            int m = 0;
             for(int i = 0; i < N-1; ++i) {
-                long long x = (long long)(arr[i] == -1 ? k : arr[i]);
-                long long y = (long long)(arr[i+1] == -1) ? k : arr[i+1];
-                long long diff = absl(x - y);
-                m = (m < diff) ? diff : m; 
+                int x = (arr[i] == -1) ? k : arr[i];
+                int y = (arr[i+1] == -1) ? k : arr[i+1];
+                int diff = absl(x - y);
+                m = max(m, diff);
             }
-            //printf("v: ");
-            //for(int i = 0; i < count; ++i)
-            //    printf("%d ", v[i].first);
-            //printf("\n");
-            printf("%lld %lld\n", m, k);
+            printf("%d %d\n", m, k);
         }
-        v.clear();
     }
     return 0;
 }
