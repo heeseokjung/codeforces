3af453d0ea370c39519c30d6ee641a6c22ad68be
hsjeong-desktop <hsjeong.cs@gmail.com>
2020-03-15 01:15:24 -0700

    2020/03/15

--- R628DIV2/C.cpp
+++ R628DIV2/C.cpp
@@ -4,14 +4,37 @@
 using namespace std;
 
 int N;
+int count = 0;
 vector<pair<int, int> > adj[100001];
+int degree[100001];
+int ans[100000];
 
 int main() {
     scanf("%d", &N);
     for(int i = 0; i < N-1; ++i) {
         int u, v;
         scanf("%d %d", &u, &v);
-        adj[u].push_back(pair<int, int>(v, -1));
+        adj[u].push_back(pair<int, int>(v, i));
+        adj[v].push_back(pair<int, int>(u, i));
+        degree[u]++;
+        degree[v]++;
+    }
+    for(int i = 0; i < N-1; ++i)
+        ans[i] = -1;
+    for(int i = 1; i <= N; ++i) {
+        if(degree[i] >= 3) {
+            for(int j = 0; j < 3; ++j) {
+                int pos = adj[i][j].second;
+                ans[pos] = count++;
+            }
+            break;
+        }
+    }
+    for(int i = 0; i < N-1; ++i) {
+        if(ans[i] == -1)
+            printf("%d\n", count++);
+        else
+            printf("%d\n", ans[i]);
     }
     return 0;
 }
