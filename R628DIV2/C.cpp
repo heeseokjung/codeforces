57c82a9c1c76101d6272e83cc67ddf8af23038e5
heeseok <hycse17@gmail.com>
2020-03-14 23:54:12 -0700

    2020/03/15

--- R628DIV2/C.cpp
+++ R628DIV2/C.cpp
@@ -3,7 +3,15 @@
 #include <vector>
 using namespace std;
 
+int N;
+vector<pair<int, int> > adj[100001];
+
 int main() {
     scanf("%d", &N);
+    for(int i = 0; i < N-1; ++i) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u].push_back(pair<int, int>(v, -1));
+    }
     return 0;
 }
