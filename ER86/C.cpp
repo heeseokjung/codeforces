a7ea212e510a23799aeb6ea6835aa53126920806
heeseokjung <hsjeong.cs@gmail.com>
2020-04-27 23:54:53 -0700

    2020/04/28

--- ER86/C.cpp
+++ ER86/C.cpp
@@ -3,17 +3,36 @@
 #include <vector>
 using namespace std;
 
+long long a, b, q;
+long long sum[40010];
+
+long long query(long long x) {
+    long long count = x / (a*b);
+    long long r = x % (a*b);
+    return sum[a*b-1]*count + sum[r];
+}
+
 int main() {
     int TC;
     scanf("%d", &TC);
     while(TC--) {
-        long long a, b, q;
         scanf("%lld %lld %lld", &a, &b, &q);
-        vector<pair<long long, long long> > query(q);
+        vector<pair<long long, long long> > v(q);
         for(int i = 0; i < q; ++i)
-            scanf("%lld %lld", &query[i].first, &query[i].second);
+            scanf("%lld %lld", &v[i].first, &v[i].second);
 
-        
+        for(int i = 0; i < a*b; ++i)
+            sum[i] = 0;
+        for(int i = 0; i < a*b; ++i) {
+            sum[i] = (long long)((i%a)%b != (i%b)%a);
+            if(i > 0)
+                sum[i] += sum[i-1];
+        }
+
+        for(int i = 0; i < q; ++i) 
+            printf("%lld ", query(v[i].second) - query(v[i].first - 1));
+        printf("\n");
     }
+
     return 0;
 }
