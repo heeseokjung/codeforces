d3aaa3a5184747841288617c1dc2d20ca09e03f3
heeseokjung <hsjeong.cs@gmail.com>
2020-05-26 22:24:58 -0700

    2020/05/27

--- R645DIV2/B.cpp
+++ R645DIV2/B.cpp
@@ -1,54 +1,29 @@
-#include <iostream>
+#include <cstdio>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
     int TC;
-    cin >> TC;
+    scanf("%d", &TC);
     while(TC--) {
         int N;
-        cin >> N;
+        scanf("%d", &N);
         vector<int> v(N);
         for(int i = 0; i < N; ++i)
-            cin >> v[i];
+            scanf("%d", &v[i]);
 
-        vector<int> count(200001, 0);
-        vector<int> sorted_v;
-        for(int i = 0; i < N; ++i) {
-            if(count[v[i]] == 0) {
-                count[v[i]]++;
-                sorted_v.push_back(v[i]);
-            } else {
-                count[v[i]]++;
+        sort(v.begin(), v.end());
+
+        int i;
+        for(i = N-1; i >= 0; --i) {
+            if(v[i] <= i + 1) {
+                printf("%d\n", i + 2);
+                break;
             }
         }
-
-        sort(sorted_v.begin(), sorted_v.end());
-
-        long long sum = 1;
-        for(int i = 0; i < (int)sorted_v.size(); ) {
-            if((long long)sorted_v[i] <= sum + (long long)count[sorted_v[i]] - 1) {
-                sum += count[sorted_v[i]];
-                i++;
-            } else {
-                long long cur = 0;
-                int j;
-                for(j = i; j < (int)sorted_v.size(); ++j) {
-                    cur += count[sorted_v[j]];
-                    if((long long)sorted_v[j] <= sum + cur - 1) {
-                        sum += cur;
-                        break;
-                    }
-                }
-                i = j+1;
-            }
-        }
-        
-        cout << sum << "\n";
+        if(i < 0)
+            printf("1\n");
     }
     return 0;
 }
